62abc432bf9e7a39a2f8f39f3073829234541af3
BugX <dhaouiaziz13@gmail.com>
2021-09-27 16:14:23 +0100

    build 1.0

--- front/components/Search.js
+++ front/components/Search.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import {
+  ScrollView,
   View,
   Text,
   TouchableOpacity,
@@ -8,9 +9,13 @@ import {
   Image,
   BackHandler,
 } from "react-native";
-// import { Route } from "react-router-native";
+
+import { apiurl } from "../vars";
+import axios from "axios";
 export default ({ history }) => {
   const [text, settext] = useState("");
+  const [data, setdata] = useState([]);
+
   useEffect(() => {
     const backHandler = BackHandler.addEventListener(
       "hardwareBackPress",
@@ -21,6 +26,21 @@ export default ({ history }) => {
     history.push("/");
     return true;
   };
+
+  /////////////////////////////////////
+  const getdata = async (keyword) => {
+    await axios
+      .post(apiurl + "/stocks/find", { word: keyword })
+      .then((res) => {
+        // console.log(res.data)
+        setdata(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  /////////////////////////////////////
   return (
     <View style={styles.container}>
       <View style={styles.inp}>
@@ -31,9 +51,28 @@ export default ({ history }) => {
           placeholderTextColor="white"
           placeholder="search"
         />
-        <Image style={styles.img} source={require("../assets/bitcoin.png")} />
+        <TouchableOpacity
+          onPress={() => {
+            getdata(text);
+          }}
+        >
+          <Image
+            style={styles.img}
+            source={require("../assets/outline_search_white_24dp.png")}
+          />
+        </TouchableOpacity>
       </View>
-      <View style={styles.body}></View>
+      <ScrollView contentContainerStyle={styles.body}>
+        {data.map((item, i) => {
+          return (
+            <TouchableOpacity onPress={() => history.push(`/data/${item.Symbol}`)} key={i}>
+              <View style={styles.singlecard} >
+                <Text style={{ fontSize: 12 }}>{item.Name.join(" ")}</Text>
+              </View>
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 };
@@ -42,28 +81,47 @@ const styles = StyleSheet.create({
   inp: {
     // borderWidth: 1,
     // borderColor: "#fff",
-    width: "100%",
+    width: "90%",
     display: "flex",
     flexDirection: "row",
-    alignItems:"center"
+    alignItems: "center",
+    padding: "1%",
+  },
+  singlecard: {
+    borderWidth: 1,
+    borderColor: "white",
+    marginVertical: 10,
+    width: 350,
+    borderRadius: 7,
+    backgroundColor: "white",
+    height: 70,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 6,
   },
   input: {
-   borderWidth:0.5,
+    borderWidth: 0.5,
     padding: 5,
-    display:"flex",
-    justifyContent:"space-around",
+    display: "flex",
+    justifyContent: "space-around",
     width: "88%",
-    marginHorizontal:5,
-    borderColor:"white",
-    borderRadius:6
+    marginHorizontal: 5,
+    marginVertical: 10,
+    borderColor: "white",
+    borderRadius: 6,
+    color: "white",
   },
   body: {
-    height: "90%",
     // borderColor: "red",
     // borderWidth: 1,
     width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
   },
-  img:{
+
+  img: {
     width: 30,
     height: 30,
   },
--- front/vars.js
+++ front/vars.js
@@ -1,1 +1,1 @@
-module.exports = { apiurl:"http://146.59.198.65:5000" };
+module.exports = {localapi:"http://88c9-41-224-0-80.ngrok.io" ,apiurl:"http://146.59.198.65:5000" };
